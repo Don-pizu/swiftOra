8591af84b2cf53ee6333c763ad60d5ce8e5a82ab
Asiru Adedolapo <105151400+Don-pizu@users.noreply.github.com>
2026-04-06 11:41:53 +0100

    Feats: Module Typescript

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MeetingsModule } from './meetings/meetings.module';
+import { ParticipantsModule } from './participants/participants.module';
+import { CommonModule } from './common/common.module';
+import { HealthController } from './health/health.controller';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
+
+    AuthModule,
+    UsersModule,
+    MeetingsModule,
+    ParticipantsModule,
+    CommonModule,
+  ],
+  controllers: [HealthController],
 })
 export class AppModule {}
